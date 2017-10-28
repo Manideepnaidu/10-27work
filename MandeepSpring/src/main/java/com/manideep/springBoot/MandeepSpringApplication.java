abb8d2637f77db1788ee87543f1da148cbd38946
Manideepnaidu <manideepnaidu77@gmail.com>
2017-10-27 20:55:03 -0500

    firsttry

---
+++ MandeepSpring/src/main/java/com/manideep/springBoot/MandeepSpringApplication.java
@@ -0,0 +1,12 @@
+package com.manideep.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandeepSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandeepSpringApplication.class, args);
+	}
+}
